9939addc391ec5d57ae64746516ac71a8f98fa4e
hemanttiwary <hemant.appu@gmail.com>
2024-09-04 20:50:13 +0530

    Added all c files

--- test18.c
+++ test18.c
@@ -2,22 +2,32 @@
 #include<stdio.h>
 
 int main(){
+    
     printf("Enter any number to make Table..");
     int num;
     scanf("%d", &num);
+
      
     for(int i=1; i<=10; i++){
         //int num2 = num * i;
         printf("%d\n", num * i);
     }
+
     int j = 100;
-    while(j<=2000){
+    while(j<=200){
         printf(": --> %d\n",j);
         j++;
         printf("\a");
         
     }
-    
+
+    int k = 300;
+    do{
+        printf(": - >>> %d\n",k);
+        k++;
+    }while(k <=400);
+    printf("End of loop.....");
+
     return 0;
 }
 

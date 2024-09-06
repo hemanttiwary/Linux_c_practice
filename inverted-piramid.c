8265a933e232420fa3ba50aef3350372711b0fc0
hemanttiwary <hemant.appu@gmail.com>
2024-09-06 15:44:32 +0530

    Added some more

--- inverted-piramid.c
+++ inverted-piramid.c
@@ -1,32 +1,18 @@
+// Right angle inverted piramid ...
+
 #include <stdio.h>
 
-int main() {
+int main()
+{
+    int i, j;
 
-int rows, i, j, space;
-
-   printf ("Enter the number of rows: ");
-
-   scanf ("%d", &rows);
-
-   for (i = rows; i >= 1; --i) {
-
-      for ( space = 0; space < rows-i; ++space )
+    for(i=0; i<=10; i++)
+    {
+        for(j=10; j>=i; j--)
         {
-             printf ("  ");
-              for (j = i; j <= 2 * i - 1; ++j)
-                {
-                    printf (" * ");
-                }
+            printf("*");
         }
-
-      for(j = 0; j < i-1; ++j)
-        {
-         printf(" * ");
-        }
-
-      printf("\n");
-
-   }
-
-   return 0;
+        printf("\n");
+    }
+  return 0;
 }
---
+++ triangle.c
@@ -0,0 +1,14 @@
+//right angle triangle piramid......
+
+#include<stdio.h>
+
+int main(){
+    int i, j;
+    for(i=0; i<=10; i++){
+        for(j=1; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+}
